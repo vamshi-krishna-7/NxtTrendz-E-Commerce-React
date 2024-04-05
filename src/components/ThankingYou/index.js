4528b8f7d6434f2d3c48ce275743fcb2a13fad09
vamshi-krishna-7 <bvamshikrishna50@gmail.com>
2024-04-05 04:41:50 +0000

    commit

---
+++ src/components/ThankingYou/index.js
@@ -0,0 +1,19 @@
+import './index.css'
+
+const ThankingYou = () => (
+  <div className="thank-you-container">
+    <img
+      src="https://www.modgility.com/hubfs/thank-you-examples-featured-image.jpg"
+      alt="thank"
+      className="image"
+    />
+    <p className="connect-text">
+      Thank you for visiting our website! Your presence means the world to us.
+      We hope you found what you were looking for and had a pleasant experience
+      browsing through our offerings. Your support is truly appreciated, and we
+      look forward to serving you again soon!
+    </p>
+  </div>
+)
+
+export default ThankingYou

f9f5b82dd9b33cc02b6791a7ba2bf2b55720e82d
vamshi-krishna-7 <bvamshikrishna50@gmail.com>
2024-08-06 14:12:14 +0000

    commit

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,6 +1,7 @@
-// Write your code here
+import Popup from 'reactjs-popup'
 
-import {Link} from 'react-router-dom'
+import Payment from '../Payment'
+
 import CartContext from '../../context/CartContext'
 
 import './index.css'
@@ -9,31 +10,32 @@ const CartSummary = () => (
   <CartContext.Consumer>
     {value => {
       const {cartList} = value
-      let total = 0
-      cartList.forEach(eachCartItem => {
-        total += eachCartItem.price * eachCartItem.quantity
-      })
-
+      const items = cartList.length
+      const totalPrice = cartList.reduce(
+        (acc, item) => acc + item.quantity * item.price,
+        0,
+      )
       return (
-        <>
-          <div className="cart-summary-container">
-            <h1 className="order-total-value">
-              <span className="order-total-label">Order Total:</span> Rs {total}{' '}
-              /-
+        <div className="cart-summary-container">
+          <div className="cart-summary-card">
+            <h1 className="cart-items-total-price">
+              Order Total: <span>RS {totalPrice}/-</span>
             </h1>
-            <p className="total-items">{cartList.length} Items in cart</p>
-            <Link to="/thankyou" className="link-button-sm">
-              <button type="button" className="checkout-button d-sm-none">
-                Checkout
-              </button>
-            </Link>
+            <p className="cart-items-count">{items} Items in cart</p>
+
+            <Popup
+              modal
+              trigger={
+                <button className="checkout-button" type="button">
+                  Checkout
+                </button>
+              }
+              position="top left"
+            >
+              {close => <Payment close={close} />}
+            </Popup>
           </div>
-          <Link to="/thankyou" className="link-button-lg">
-            <button type="button" className="checkout-button d-lg-none">
-              Checkout
-            </button>
-          </Link>
-        </>
+        </div>
       )
     }}
   </CartContext.Consumer>
